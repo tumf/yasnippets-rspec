b8f5966075b312358075aa27def233fd118623a1
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2013-07-08 16:20:14 +0900

    remove yas/initialize

--- setup.el
+++ setup.el
@@ -1,6 +1,5 @@
 ;;; setup.el --- setup yasnippets for use with rspec
 (require 'yasnippet)
-(yas/initialize)
 (yas/load-directory
  (concat (file-name-directory (or load-file-name buffer-file-name))
 	 "rspec-snippets/"))
